5f8d2c0d3b392ed7120044f86376961a391afe33
Branch Vincent <branchevincent@gmail.com>
2024-06-16 16:41:45 -0700

    scikit-image: revision bump (numpy 2.0.0)

--- Formula/s/scikit-image.rb
+++ Formula/s/scikit-image.rb
@@ -6,6 +6,7 @@ class ScikitImage < Formula
   url "https://files.pythonhosted.org/packages/24/ce/183ff64ed397911a9d3b671714f8a2618af407b427a40ca48550fb0f7bd7/scikit_image-0.23.2.tar.gz"
   sha256 "c9da4b2c3117e3e30364a3d14496ee5c72b09eb1a4ab1292b302416faa360590"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/scikit-image/scikit-image.git", branch: "main"
 
   bottle do
